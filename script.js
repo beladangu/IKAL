f776ade92c4da3d7a01661d95718297a6d05c52e
beladangu <125761776+beladangu@users.noreply.github.com>
2024-08-12 20:12:14 -0600

    script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let currentIndex = 0;
 
   function updateTextAndImage() {
-    carouselItems.forEach((item) => {
+    carouselItems.forEach((item, index) => {
       item.classList.remove('active'); // Eliminar clase activa de todas las imágenes
     });
 
@@ -54,42 +54,3 @@ document.addEventListener('DOMContentLoaded', () => {
   // Inicializar el carrusel
   carouselItems[currentIndex].classList.add('active');
 });
-
-    { word: 'BIENVENIDO', subtext: 'Estas en el lugar donde cada tratamiento es una conexión con la naturaleza y la sanación.', image: 'imagenes/BIENVENIDO.png' },
-    { word: 'ARMONÍA', subtext: '"Ofrecemos servicios integrales para la sanación del cuerpo, el espíritu y el alma por medio de terapia holística y la medicina tradicional."', image: 'imagenes/ARMONÍA.jpg' },
-    { word: 'SALUD', subtext: '"Una experiencia que te permite conectar contigo mism@, la naturaleza y fluir en un abrazo con tus emociones."', image: 'imagenes/SALUD.jpg' },
-    { word: 'SERVICIOS', subtext: '"Auriculoterapia, acupuntura, moxibustión, ventosas, tratamientos herbolarios, flores de bach y más."', image: 'imagenes/SERVICIOS.jpg' }
-  ];
-
-  function updateTextAndImage() {
-    carouselItems.forEach((item, index) => {
-      item.classList.remove('active'); // Eliminar clase activa de todas las imágenes
-    });
-
-    currentIndex = (currentIndex + 1) % combinations.length;
-    dynamicText.textContent = combinations[currentIndex].word;
-    dynamicSubtext.textContent = combinations[currentIndex].subtext;
-    carouselItems[currentIndex].style.backgroundImage = `url(${combinations[currentIndex].image})`;
-
-    carouselItems[currentIndex].classList.add('active'); // Agregar clase activa a la imagen actual
-  }
-
-  setInterval(updateTextAndImage, 7000); // Cambiar intervalo a 7000ms (7 segundos)
-
-  // Ajustar la visibilidad del contenido en móvil
-  const updateMenuOnResize = () => {
-    const navbarNav = document.querySelector('.navbar-nav');
-    if (window.innerWidth <= 768) {
-      navbarNav.style.display = 'block';
-    } else {
-      navbarNav.style.display = 'flex';
-    }
-  };
-
-  window.addEventListener('resize', updateMenuOnResize);
-  updateMenuOnResize(); // Llamar al iniciar
-
-  // Inicializar el carrusel
-  carouselItems[currentIndex].classList.add('active');
-});
-
