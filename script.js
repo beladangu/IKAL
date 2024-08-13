20d5a638e157f528dbc3c7b549174391cd0dd4be
beladangu <125761776+beladangu@users.noreply.github.com>
2024-08-12 19:39:06 -0600

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Actualización del texto dinámico
   const dynamicText = document.getElementById('dynamic-text');
   const dynamicSubtext = document.getElementById('dynamic-subtext');
   const items = document.querySelectorAll('.carousel-item');
-  
+
   const combinations = [
     { word: 'BIENVENIDO', subtext: 'Estas en el lugar donde cada tratamiento es una conexión con la naturaleza y la sanación.', image: 'imagenes/BIENVENIDO.png' },
-    { word: 'ARMONÍA', subtext: '"Ofrecemos servicios integrales para la sanación del cuerpo, el espíritu y el alma por medio de terapia holística y la medicina tradicional."', image: 'imagenes/ARMONÍA.jpg' },
-    { word: 'SALUD', subtext: '"Una experiencia que te permite conectar contigo mism@, la naturaleza y fluir en un abrazo con tus emociones."', image: 'imagenes/SALUD.jpg' },
-    { word: 'SERVICIOS', subtext: '"Auriculoterapia, acupuntura, moxibustión, ventosas, tratamientos herbolarios, flores de bach y más."', image: 'imagenes/SERVICIOS.jpg' }
+    { word: 'ARMONÍA', subtext: 'Ofrecemos servicios integrales para la sanación del cuerpo, el espíritu y el alma por medio de terapia holística y la medicina tradicional.', image: 'imagenes/ARMONÍA.jpg' },
+    { word: 'SALUD', subtext: 'Una experiencia que te permite conectar contigo mism@, la naturaleza y fluir en un abrazo con tus emociones.', image: 'imagenes/SALUD.jpg' },
+    { word: 'SERVICIOS', subtext: 'Auriculoterapia, acupuntura, moxibustión, ventosas, tratamientos herbolarios, flores de bach y más.', image: 'imagenes/SERVICIOS.jpg' }
   ];
 
   let currentIndex = 0;
 
   function updateTextAndImage() {
     items.forEach((item, index) => {
-      item.classList.remove('active'); // Eliminar clase activa de todas las imágenes
+      item.classList.remove('active');
     });
 
     currentIndex = (currentIndex + 1) % combinations.length;
@@ -23,12 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
     dynamicSubtext.textContent = combinations[currentIndex].subtext;
     items[currentIndex].style.backgroundImage = `url(${combinations[currentIndex].image})`;
 
-    items[currentIndex].classList.add('active'); // Agregar clase activa a la imagen actual
+    items[currentIndex].classList.add('active');
   }
 
-  setInterval(updateTextAndImage, 7000); // Cambiar intervalo a 7000ms (7 segundos)
+  setInterval(updateTextAndImage, 7000);
 
-  // Reproducción de música
   const music = document.getElementById('background-music');
   const playMusicBtn = document.getElementById('play-music-btn');
 
@@ -37,6 +35,5 @@ document.addEventListener('DOMContentLoaded', () => {
     playMusicBtn.style.display = 'none';
   });
 
-  // Inicializar el carrusel
   items[currentIndex].classList.add('active');
 });
