89371bbff8cb882dc9ea812a4758e719b60934fe
beladangu <125761776+beladangu@users.noreply.github.com>
2024-08-12 19:59:32 -0600

    script.js

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const dynamicText = document.getElementById('dynamic-text');
-  const dynamicSubtext = document.getElementById('dynamic-subtext');
-  const items = document.querySelectorAll('.carousel-item');
   const music = document.getElementById('background-music');
   const playMusicBtn = document.getElementById('play-music-btn');
- const combinations = [
-    { word: 'BIENVENIDO', subtext: 'Estas en el lugar donde cada tratamiento es una conexión con la naturaleza y la sanación.', image: 'imagenes/BIENVENIDO.png' },
-    { word: 'ARMONÍA', subtext: '"Ofrecemos servicios integrales para la sanación del cuerpo, el espíritu y el alma por medio de terapia holística y la medicina tradicional."', image: 'imagenes/ARMONÍA.jpg' },
-    { word: 'SALUD', subtext: '"Una experiencia que te permite conectar contigo mism@, la naturaleza y fluir en un abrazo con tus emociones."', image: 'imagenes/SALUD.jpg' },
-    { word: 'SERVICIOS', subtext: '"Auriculoterapia, acupuntura, moxibustión, ventosas, tratamientos herbolarios, flores de bach y más."', image: 'imagenes/SERVICIOS.jpg' }
-  ];
-
-  let currentIndex = 0;
-
-  function updateTextAndImage() {
-    items.forEach((item, index) => {
-      item.classList.remove('active'); // Eliminar clase activa de todas las imágenes
-    });
-
-    currentIndex = (currentIndex + 1) % combinations.length;
-    dynamicText.textContent = combinations[currentIndex].word;
-    dynamicSubtext.textContent = combinations[currentIndex].subtext;
-    items[currentIndex].style.backgroundImage = `url(${combinations[currentIndex].image})`;
-
-    items[currentIndex].classList.add('active'); // Agregar clase activa a la imagen actual
-  }
-
-  setInterval(updateTextAndImage, 7000); // Cambiar intervalo a 7000ms (7 segundos)
 
   // Reproducción y ocultación del botón de música
   playMusicBtn.addEventListener('click', () => {
@@ -54,6 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('.navbar-nav').style.display = 'flex';
     }
   };
+
   window.addEventListener('resize', updateMenuOnResize);
   updateMenuOnResize(); // Llamar al iniciar
 });
