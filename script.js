ca42dcb47d3c3ebed865cd0f55395faba778ddcc
beladangu <125761776+beladangu@users.noreply.github.com>
2024-08-12 20:02:33 -0600

    script.js

--- script.js
+++ script.js
@@ -20,15 +20,46 @@ document.addEventListener('DOMContentLoaded', () => {
 
   setInterval(showNextItem, 7000); // Cambia cada 7 segundos
 
+  // Actualización del texto dinámico
+  const dynamicText = document.getElementById('dynamic-text');
+  const dynamicSubtext = document.getElementById('dynamic-subtext');
+
+  const combinations = [
+    { word: 'BIENVENIDO', subtext: 'Estas en el lugar donde cada tratamiento es una conexión con la naturaleza y la sanación.', image: 'imagenes/BIENVENIDO.png' },
+    { word: 'ARMONÍA', subtext: '"Ofrecemos servicios integrales para la sanación del cuerpo, el espíritu y el alma por medio de terapia holística y la medicina tradicional."', image: 'imagenes/ARMONÍA.jpg' },
+    { word: 'SALUD', subtext: '"Una experiencia que te permite conectar contigo mism@, la naturaleza y fluir en un abrazo con tus emociones."', image: 'imagenes/SALUD.jpg' },
+    { word: 'SERVICIOS', subtext: '"Auriculoterapia, acupuntura, moxibustión, ventosas, tratamientos herbolarios, flores de bach y más."', image: 'imagenes/SERVICIOS.jpg' }
+  ];
+
+  function updateTextAndImage() {
+    carouselItems.forEach((item, index) => {
+      item.classList.remove('active'); // Eliminar clase activa de todas las imágenes
+    });
+
+    currentIndex = (currentIndex + 1) % combinations.length;
+    dynamicText.textContent = combinations[currentIndex].word;
+    dynamicSubtext.textContent = combinations[currentIndex].subtext;
+    carouselItems[currentIndex].style.backgroundImage = `url(${combinations[currentIndex].image})`;
+
+    carouselItems[currentIndex].classList.add('active'); // Agregar clase activa a la imagen actual
+  }
+
+  setInterval(updateTextAndImage, 7000); // Cambiar intervalo a 7000ms (7 segundos)
+
   // Ajustar la visibilidad del contenido en móvil
   const updateMenuOnResize = () => {
+    const navbarNav = document.querySelector('.navbar-nav');
     if (window.innerWidth <= 768) {
-      document.querySelector('.navbar-nav').style.display = 'block';
+      navbarNav.style.display = 'block';
     } else {
-      document.querySelector('.navbar-nav').style.display = 'flex';
+      navbarNav.style.display = 'flex';
     }
   };
 
   window.addEventListener('resize', updateMenuOnResize);
   updateMenuOnResize(); // Llamar al iniciar
+
+  // Inicializar el carrusel
+  carouselItems[currentIndex].classList.add('active');
 });
+
